517d193fcb3b74709e9c5ac9664675cfb62bbb59
pasovacoin <pasovacoin@gmail.com>
2025-04-17 08:10:17 +0300

    Update airdrop-template.sh

--- scripts/airdrop-template.sh
+++ scripts/airdrop-template.sh
@@ -9,8 +9,8 @@ addresses=(
   "WalletAddress3"
 )
 
-token_mint="CMtGP3edzwQ3rZFbQnZDimjQZDXGNkBBLbH3NEDDa8wF"
-amount=100000000 # 100 MESE with 9 decimals
+token_mint="8srRUfYmFC3GCf9EDCrwpDP1Dv2J8jf6b1q63q7kpump"
+amount=1000000000 # 1000 MESE with 6 decimals
 
 for addr in "${addresses[@]}"; do
   echo "Airdropping 100 MESE to $addr..."
